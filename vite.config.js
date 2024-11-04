359e8e9e2fcfdba8f2eb9427f299877b69cbfe58
clark-cui <cuirongchuan@163.com>
2024-11-04 18:39:47 +0800

    feat: dapp demo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: false,
+    terserOptions: false,
+  },
+  rollupOptions: {
+    // 配置输出格式，'es' 为 ES module，'cjs' 为 CommonJS
+    output: {
+      format: "es",
+      // 不生成sourcemap
+      sourcemap: false,
+    },
+  },
+});
