3cde79f71303db50c372cffd47e5ee60e0e23a3d
clark-cui <cuirongchuan@163.com>
2024-11-04 16:09:49 +0800

    feat: add personal sign

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState, useRef, useEffect, useMemo } from "react";
-import { Button } from "antd";
+import { Button, Input } from "antd";
 import { OKXUniversalConnectUI, THEME } from "@okxconnect/ui";
+import hexer from "browser-string-hexer";
 
 import "./App.css";
 
 function App() {
   const universalUi = useRef(null);
   const [isConnected, setIsConnected] = useState(false);
+  const [signResult, setSignResult] = useState("");
+  const [signText, setSignText] = useState("");
   const initFunc = async () => {
     universalUi.current = await OKXUniversalConnectUI.init({
       dappMetaData: {
@@ -23,6 +26,10 @@ function App() {
         theme: THEME.LIGHT,
       },
     });
+
+    if (universalUi.current.session) {
+      setIsConnected(true);
+    }
   };
 
   useEffect(() => {
@@ -57,35 +64,50 @@ function App() {
     console.log(session, "connected");
   };
 
-  const text = useMemo(() => {
+  const [text, text2] = useMemo(() => {
     if (!universalUi?.current?.session) {
-      return `Connect Wallet`;
+      return [`Connect Wallet`, "no wallet"];
     }
-    return "Conected";
+    return [
+      "Conected",
+      `wallet address is ${universalUi?.current?.session?.namespaces?.accounts[0]}`,
+    ];
   }, [universalUi?.current?.session]);
 
   const handlePersonalSign = async () => {
     const chain = "eip155:43114";
     const data = {
       method: "personal_sign",
-      params: [
-        "0x506c65617365207369676e2074686973206d65737361676520746f20636f6e6669726d20796f7572206964656e746974792e",
-      ],
+      params: [hexer.utf8ToHex(signText)],
     };
     const personalSignResult = await universalUi.current?.request(
       data,
       chain,
       "all"
     );
+    setSignResult(personalSignResult);
     console.log(personalSignResult, "personalSignResult");
+  };
+  const handleTextInput = (e) => {
+    setSignResult(e.target.value);
   };
   return (
     <>
       <h1 className="title">Dapp Demo</h1>
-      <Button type="primary" onClick={handleConnect}>
+      <h3>{text2}</h3>
+      <Button type="primary" onClick={handleConnect} disabled={isConnected}>
         {text}
       </Button>
+      <br />
+      <Input
+        placeholder="sign message"
+        value={signText}
+        onChange={handleTextInput}
+      />
       <Button onClick={handlePersonalSign}>personal sign</Button>
+      <br />
+      <h3>Sign Result</h3>
+      <textarea>{signResult}</textarea>
     </>
   );
 }
