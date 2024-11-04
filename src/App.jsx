bb0f90d24157e514d5117c6aa62b1f01204fcdf6
clark-cui <cuirongchuan@163.com>
2024-11-04 15:16:46 +0800

    feat: add personal sign

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,10 @@ function App() {
   };
 
   useEffect(() => {
-   const scriptTag =  document.createElement("script");
-    scriptTag.src = "https://telegram.org/js/telegram-web-app.js";
-    scriptTag.id = "tgsdk";
-    document.getElementsByTagName('head')[0].appendChild(scriptTag);
+    //  const scriptTag =  document.createElement("script");
+    //   scriptTag.src = "https://telegram.org/js/telegram-web-app.js";
+    //   scriptTag.id = "tgsdk";
+    //   document.getElementsByTagName('head')[0].appendChild(scriptTag);
     initFunc();
   }, []);
 
@@ -64,12 +64,28 @@ function App() {
     return "Conected";
   }, [universalUi?.current?.session]);
 
+  const handlePersonalSign = async () => {
+    const chain = "eip155:1";
+    const data = {
+      method: "personal_sign",
+      params: [
+        "0x506c65617365207369676e2074686973206d65737361676520746f20636f6e6669726d20796f7572206964656e746974792e",
+      ],
+    };
+    const personalSignResult = await universalUi.current?.request(
+      data,
+      chain,
+      "all"
+    );
+    console.log(personalSignResult, "personalSignResult");
+  };
   return (
     <>
       <h1 className="title">Dapp Demo</h1>
       <Button type="primary" onClick={handleConnect}>
         {text}
       </Button>
+      <Button onClick={handlePersonalSign}>personal sign</Button>
     </>
   );
 }
